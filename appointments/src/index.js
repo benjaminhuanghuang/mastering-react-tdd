af522614701cf8e21f5614ee4c09d680f3cbddc2
Benjamin Huang <bhuang@rms.com>
2019-12-31 11:16:09 -0800

    fix router

--- appointments/src/index.js
+++ appointments/src/index.js
@@ -1,9 +1,13 @@
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App';
+import { Router, Route } from 'react-router';
+import { appHistory } from './history';
+import { App } from './App';
 
 ReactDOM.render(
-  <App />,
+  <Router history={appHistory}>
+    <Route path="/" component={App} />
+  </Router>,
   document.getElementById('root')
 );
